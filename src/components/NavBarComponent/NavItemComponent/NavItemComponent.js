79ec0fbfdf728aed80b591f90574d34fe6c8396f
Zamar Roura <zamarfazal@gmail.com>
2021-12-08 21:35:53 +0100

    last push

--- src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
+++ src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import {isMobile} from 'react-device-detect'
 
 const NavItemComponent = (props) => (
-  <div className={styles.NavItemComponent}>
+  <div className={styles.NavItemComponent +(props.name !="HOME" ? ' ' + styles.BorderLeft:'')}>
     {!isMobile ? <img src={props.src} alt={props.name + " icon"}></img>:''}
     <NavLink to={props.to}>{props.name}</NavLink>
   </div>
