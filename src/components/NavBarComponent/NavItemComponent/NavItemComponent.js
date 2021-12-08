26e236adc655f81a76a525ab9018ef15162426c4
Zamar Roura <zamarfazal@gmail.com>
2021-12-08 23:01:09 +0100

    navitem photo

--- src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
+++ src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
@@ -5,7 +5,7 @@ import {isMobile} from 'react-device-detect'
 
 const NavItemComponent = (props) => (
   <div className={styles.NavItemComponent +(props.name !=="HOME" ? ' ' + styles.BorderLeft:'')}>
-    {!isMobile ? <img src={props.src} alt={props.name + " icon"}></img>:''}
+    <img src={props.src} width ="20px" alt={props.name + " icon"}></img>
     <NavLink to={props.to}>{props.name}</NavLink>
   </div>
 );
