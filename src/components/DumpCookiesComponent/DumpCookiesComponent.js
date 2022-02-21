192f11c71a44bc6bf9ea5282a277c54a1820f26d
Zamar Roura <zamar.roura@prensaiberica.es>
2022-02-21 17:53:17 +0100

    Dump cookies

--- src/components/DumpCookiesComponent/DumpCookiesComponent.js
+++ src/components/DumpCookiesComponent/DumpCookiesComponent.js
@@ -7,15 +7,14 @@ import { useLocation } from 'react-router-dom';
 const DumpCookiesComponent = () => {
   const location = useLocation();
   React.useEffect(pageview(location.pathname));
+  useEffect(() => {
+    document.getElementById("d").innerHTML = "<b>document.cookie:</b> " + document.cookie
+  });
   return (
   <div className={styles.DumpCookiesComponent}>
      The following cookies have been set:
-    <h1>
-      Member 1
-    </h1>
     <div id="d"></div>
-    <script>document.getElementById("d").innerHTML = "<b>document.cookie:</b> " + document.cookie</script>
-    <a href="/">Home</a>
+    <a href="/firstpartyset">Home</a>
   </div>
 );}
 
