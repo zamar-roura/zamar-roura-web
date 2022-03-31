848cc786452b61774ac65e52585f08184a020ccf
Zamar Roura <zamarfazal@gmail.com>
2022-04-01 01:21:59 +0200

    No cookies on page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import HeaderComponent from './components/HeaderComponent/HeaderComponent';
 import HomeComponent from './components/HomeComponent/HomeComponent';
 import NavBarComponent from './components/NavBarComponent/NavBarComponent';
-import SetCookiesComponent from './components/SetCookiesComponent/SetCookiesComponent';
-import FirstPartySetComponent from './components/FirstPartySetComponent/FirstPartySetComponent';
-import DumpCookiesComponent from './components/DumpCookiesComponent/DumpCookiesComponent';
+
 
 
 
