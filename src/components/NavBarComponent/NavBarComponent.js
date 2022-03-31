664daf41d71d56fbefc97484e6e0b4c7b2a32303
Zamar Roura <zamarfazal@gmail.com>
2022-04-01 01:20:59 +0200

    fix

--- src/components/NavBarComponent/NavBarComponent.js
+++ src/components/NavBarComponent/NavBarComponent.js
@@ -4,7 +4,7 @@ import NavItemComponent from './NavItemComponent/NavItemComponent';
 
 const NavBarComponent = () => (
   <div className={styles.NavBarComponent}>
-      <NavItemComponent to="" name="Projects"></NavItemComponent>
+      {/* <NavItemComponent to="" name="Projects"></NavItemComponent> */}
       <NavItemComponent to="https://github.com/zamar-roura" name="Github"></NavItemComponent>
       <NavItemComponent to="https://www.linkedin.com/in/zamarelahi/" name="Linkedin"></NavItemComponent>
       <NavItemComponent to="" name="Mail"></NavItemComponent>
