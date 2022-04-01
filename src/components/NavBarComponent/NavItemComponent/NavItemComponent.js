fa65332837132827d930b0f49d2d526882e08fb5
Zamar Roura <zamarfazal@gmail.com>
2022-04-01 19:17:01 +0200

    analytics

--- src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
+++ src/components/NavBarComponent/NavItemComponent/NavItemComponent.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import styles from './NavItemComponent.module.scss';
-import ReactGA from "react-ga4";
-ReactGA.initialize([
-    {
-        trackingId: "G-Z2BVY8Y6H2",
-    }
-    ]);
-function analyticsEvent(name,href){
 
-    ReactGA.event({
-        category: 'Navigation',
-        action: "Button "+name,
-        label: href, 
-        nonInteraction: true
-      });
-}
 const NavItemComponent = (props) => (
 
-    <a href={props.to} onClick={() => analyticsEvent(props.name,props.to)} className={[styles.btn, styles.btnstripe].join(" ")}>{props.name}</a>
+    <a href={props.to} className={[styles.btn, styles.btnstripe].join(" ")}>{props.name}</a>
 );
 
 NavItemComponent.propTypes = {};
