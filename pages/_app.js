4e6fce213e9c4e85aaee000cf08dfbb43c0fb88b
Zamar Roura <zamarfazal@gmail.com>
2022-04-10 17:12:26 +0200

    app js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Zamar Roura" />
         <meta
@@ -29,8 +29,28 @@ function MyApp({ Component, pageProps }) {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
-        /> */}
-      {/* </Head> */}
+        />
+         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2BVY8Y6H2"/>
+       <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments)}
+          window.gtag('js', new Date());
+          window.gtag('config', 'G-Z2BVY8Y6H2');
+        `,
+        }}
+      />
+   
+       <link
+         href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+         rel="stylesheet"
+       />
+       <link
+         href="https://fonts.googleapis.com/css?family=Red+Hat+Mono"
+         rel="stylesheet"
+       />
+      </Head>
       <Component {...pageProps} />
     </>)
 }
