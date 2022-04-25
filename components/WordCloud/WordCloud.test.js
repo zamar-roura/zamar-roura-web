86a3e3b838284bfd00340d0eb46b47486ff73451
Zamar Roura <zamarfazal@gmail.com>
2022-04-26 00:23:55 +0200

    clean main too

--- components/WordCloud/WordCloud.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WordCloud from './WordCloud';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WordCloud />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
