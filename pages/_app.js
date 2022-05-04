45c6708505ce9c32e4f91709c8c175c22e28680f
Zamar Roura <zamarfazal@gmail.com>
2022-05-04 18:58:33 +0200

    GTM Instead

--- pages/_app.js
+++ pages/_app.js
@@ -14,10 +14,11 @@ function MyApp({ Component, pageProps }) {
        <script
         dangerouslySetInnerHTML={{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments)}
-          window.gtag('js', new Date());
-          window.gtag('config', 'G-Z2BVY8Y6H2');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NXXBDL6');
         `,
         }}
       />
