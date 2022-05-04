38a06f3a027ad7fd0042f8cac6a65c5dfe68eb5e
Zamar Roura <zamarfazal@gmail.com>
2022-05-04 02:07:14 +0200

    down again

--- pages/index.js
+++ pages/index.js
@@ -159,6 +159,7 @@ const SpotifyWordCloud = () => {
       <meta name="theme-color" content="#ffffff" />
     </Head>
         <div className='container'>
+            <h1>My server seems to be down. Try again in a bit, thank you for all the love :)</h1> 
             <h2>Find the most frequent words in the playlist lyrics</h2> 
             <h6>*It must be public and with less than 150 songs.</h6> 
             {error && <h4>{error}</h4>}
