65ff58e055b0ea518903d515912d59beab8046b6
Zamar Roura <zamarfazal@gmail.com>
2022-05-14 19:27:10 +0200

    cross origin

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
 
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2BVY8Y6H2" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7813212113166422"
-        crossorigin="anonymous"></script>
+        crossOrigin="anonymous"></script>
        <script
         dangerouslySetInnerHTML={{
           __html: `
